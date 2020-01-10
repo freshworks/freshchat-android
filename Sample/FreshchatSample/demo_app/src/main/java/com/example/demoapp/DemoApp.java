5fb5ce4ada0b688f1e299ff57a49f8b2f4daeca9
Duraivel-Perumal <duraivel.perumal@freshworks.com>
2020-01-10 17:59:19 +0530

    Added : Server Key

--- Sample/FreshchatSample/demo_app/src/main/java/com/example/demoapp/DemoApp.java
+++ Sample/FreshchatSample/demo_app/src/main/java/com/example/demoapp/DemoApp.java
@@ -78,6 +78,16 @@ public class DemoApp extends Application {
         FreshchatConfig freshchatConfig = new FreshchatConfig("APP_ID", "APP_KEY");
         getFreshchatInstance(getApplicationContext()).init(freshchatConfig);
 
+        /**
+         * This is the Firebase push notification server key for this sample app.
+         * Please save this in your Freshchat account to test push notifications in Sample app.
+         *
+         * Server Key:
+         * AAAAWQmY32o:APA91bF_X9S3rrfBLT5kUVaZ2uftEIpqALXu3z05SoTZhAikINhGmUsd62jQE374vgHLZyILA3lhsAxLJSQlivsZcK_yY_DAjdAQTesUpabs1_5XiRsonrWDm5envw24nQSPTDG81g1w
+         *
+         * Note: This is the push notification server key for sample app. You need to use your own server key for testing in your application
+         */
+
         Uri soundUri = Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.musical003);
 
         FreshchatNotificationConfig notificationConfig = new FreshchatNotificationConfig()
